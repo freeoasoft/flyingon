502344b7a4203ab6e1d52c67d63c98b55dbdc901
yaozy <freeoasoft@126.com>
2017-09-27 16:08:31 +0800

    update

--- samples/menu.zh-CN.js
+++ samples/menu.zh-CN.js
@@ -39,55 +39,55 @@ var menuList = [
     {
         text: '控件', 
         children: [
-            { text: '标签', url: url_controls + 'label.js' },
-            { text: '按钮', url: url_controls + 'button.js' },
-            { text: '链接', url: url_controls + 'linkbutton.js' },
-            { text: '图片', url: url_controls + 'image.js' },
-            { text: '滑块', url: url_controls + 'slider.js' },
-            { text: '进度条', url: url_controls + 'progressbar.js' },
-            { text: '单选框', url: url_controls + 'radiobutton.js' },
-            { text: '检查框', url: url_controls + 'checkbox.js' },
-            { text: '文本框', url: url_controls + 'textbox.js' },
-            { text: '多行文本', url: url_controls + 'memo.js' },
-            { text: '文本按钮', url: url_controls + 'textbutton.js' },
-            { text: '列表框', url: url_controls + 'listbox.js' },
-            { text: '下拉框', url: url_controls + 'combobox.js' },
-            { text: '数字编辑框', url: url_controls + 'number.js' },
-            { text: '日历', url: url_controls + 'calendar.js' },
-            { text: '日期选择', url: url_controls + 'date.js' },
-            { text: '时间选择', url: url_controls + 'time.js' },
-            { text: '年月选择', url: url_controls + 'month.js' },
-            { text: '面板', url: url_controls + 'panel.js' },
-            { text: '分组框', url: url_controls + 'groupbox.js' },
-            { text: '页签', url: url_controls + 'tab.js' },
-            { text: '树', url: url_controls + 'tree.js' },
-            { text: '弹出层', url: url_controls + 'popup.js' },
-            { text: '提示框', url: url_controls + 'tooltip.js' },
-            { text: '弹出窗口', url: url_controls + 'dialog.js' },
-            { text: '消息框', url: url_controls + 'showmessage.js' },
-            { text: '菜单', url: url_controls + 'menu.js' },
-            { text: '工具条', url: url_controls + 'toolbar.js' }
-        ]
-    },
-
-    {
-        text: '表格控件', 
-        children: [
-            { text: '多行列头', url: url_grid + 'multi-title.js' },
-            { text: '自定义列头', url: url_grid + 'custom-title.js' },
-            { text: '自定义渲染', url: url_grid + 'custom-render.js' },
-            { text: '单元格合并', url: url_grid + 'cell-merge.js' },
-            { text: '分页', url: url_grid + 'pagination.js' },
-            { text: '锁定', url: url_grid + 'locked.js' },
-            { text: '分组', url: url_grid + 'group.js' },
-            { text: '树表', url: url_grid + 'tree-grid.js' },
-            { text: '一万列测试', url: url_grid + 'demo1.js' },
-            { text: '十万行测试', url: url_grid + 'demo2.js' },
-            { text: '行号列', url: url_grid + 'column-no.js' },
-            { text: '选择列', url: url_grid + 'column-check.js' },
-            { text: '编辑列', url: url_grid + 'column-edit.js' }
+            { text: 'Label', url: url_controls + 'label.js' },
+            { text: 'Button', url: url_controls + 'button.js' },
+            { text: 'LinkButton', url: url_controls + 'linkbutton.js' },
+            { text: 'Image', url: url_controls + 'image.js' },
+            { text: 'Slider', url: url_controls + 'slider.js' },
+            { text: 'ProgressBar', url: url_controls + 'progressbar.js' },
+            { text: 'RadioButton', url: url_controls + 'radiobutton.js' },
+            { text: 'CheckBox', url: url_controls + 'checkbox.js' },
+            { text: 'TextBox', url: url_controls + 'textbox.js' },
+            { text: 'Memo', url: url_controls + 'memo.js' },
+            { text: 'TextButton', url: url_controls + 'textbutton.js' },
+            { text: 'ListBox', url: url_controls + 'listbox.js' },
+            { text: 'ComboBox', url: url_controls + 'combobox.js' },
+            { text: 'Number', url: url_controls + 'number.js' },
+            { text: 'Calendar', url: url_controls + 'calendar.js' },
+            { text: 'Date', url: url_controls + 'date.js' },
+            { text: 'Time', url: url_controls + 'time.js' },
+            { text: 'Month', url: url_controls + 'month.js' },
+            { text: 'Panel', url: url_controls + 'panel.js' },
+            { text: 'GroupBox', url: url_controls + 'groupbox.js' },
+            { text: 'Tab', url: url_controls + 'tab.js' },
+            { text: 'Tree', url: url_controls + 'tree.js' },
+            { text: 'Popup', url: url_controls + 'popup.js' },
+            { text: 'ToolTip', url: url_controls + 'tooltip.js' },
+            { text: 'Dialog', url: url_controls + 'dialog.js' },
+            { text: 'Message', url: url_controls + 'showmessage.js' },
+            { text: 'Menu', url: url_controls + 'menu.js' },
+            { text: 'ToolBar', url: url_controls + 'toolbar.js' },
+            {
+                text: 'Grid', 
+                children: [
+                    { text: '多行列头', url: url_grid + 'multi-title.js' },
+                    { text: '自定义列头', url: url_grid + 'custom-title.js' },
+                    { text: '自定义渲染', url: url_grid + 'custom-render.js' },
+                    { text: '单元格合并', url: url_grid + 'cell-merge.js' },
+                    { text: '分页', url: url_grid + 'pagination.js' },
+                    { text: '锁定', url: url_grid + 'locked.js' },
+                    { text: '分组', url: url_grid + 'group.js' },
+                    { text: '树表', url: url_grid + 'tree-grid.js' },
+                    { text: '一万列测试', url: url_grid + 'demo1.js' },
+                    { text: '十万行测试', url: url_grid + 'demo2.js' },
+                    { text: '行号列', url: url_grid + 'column-no.js' },
+                    { text: '选择列', url: url_grid + 'column-check.js' },
+                    { text: '编辑列', url: url_grid + 'column-edit.js' }
+                ]
+            }
         ]
     }
+    
 
 ];
 
